d4ea7a5e4d9f1506f9f753eb3595de3a762c75d0
jrx <jrepnak@hashicorp.com>
2025-01-16 19:45:08 +0100

    Adds Nomad job for Traefik and ACME configuration

--- nomad/main.tf
+++ nomad/main.tf
@@ -16,3 +16,32 @@ resource "nomad_job" "vault-unsealer" {
   jobspec = file(
   "${path.module}/jobs/vault-unsealer.hcl.tmpl")
 }
+
+resource "nomad_job" "http-echo" {
+  jobspec = file(
+  "${path.module}/jobs/http-echo.hcl.tmpl")
+}
+
+resource "nomad_acl_policy" "traefik_policy" {
+  name        = "traefik-policy"
+  description = "Policy for Traefik with read-job permission"
+  rules_hcl   = <<EOT
+namespace "default" {
+  policy = "read"
+  capabilities = ["read-job"]
+}
+EOT
+}
+
+resource "nomad_acl_token" "traefik_token" {
+  name     = "traefik-token"
+  type     = "client"
+  policies = [nomad_acl_policy.traefik_policy.name]
+}
+
+resource "nomad_job" "traefik" {
+  jobspec = templatefile(
+    "${path.module}/jobs/traefik.hcl.tmpl", {
+      token = nomad_acl_token.traefik_token.secret_id,
+  })
+}
